ef7eb2fbd7a52ab2004d80b17c23dff4c5a138bf
Дмитрий Мазин <mitya.mazin@mail.ru>
2023-01-11 17:58:07 +0300

    Added comments to methods

--- JsonBuilder/AddOrEditData.xaml.cs
+++ JsonBuilder/AddOrEditData.xaml.cs
@@ -17,7 +17,7 @@ using Xceed.Wpf.Toolkit;
 namespace JsonBuilder
 {
     /// <summary>
-    /// Логика взаимодействия для DataEntry.xaml
+    /// Interaction logic for DataEntry
     /// </summary>
     public partial class AddOrEditData : Window
     {
@@ -51,7 +51,7 @@ namespace JsonBuilder
             }
         }
         object line;
-        private void Edit()
+        private void Edit() // Method for displaying editable data
         {
             line = mw.personList1.SelectedItems[0];
             txtFIO.Text = (line as Person).FIO;
@@ -67,11 +67,11 @@ namespace JsonBuilder
             listCardAdd.ItemsSource = lstCard;
             listCardAdd.Items.Refresh();
         }
-        private void calendar1_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        private void calendar1_SelectedDatesChanged(object sender, SelectionChangedEventArgs e) // Method where a date is selected in the calendar
         {
             DateTime? selectedDate = calendar1.SelectedDate;
         }
-        private void Calendar1_LostFocus(object sender, RoutedEventArgs e)
+        private void Calendar1_LostFocus(object sender, RoutedEventArgs e) // A method where access is given to save data when focus is lost if all data is entered
         {
             if (txtFIO.Text != null && calendar1.SelectedDate != null && (rbtnMan.IsChecked == true || rbtnWoman.IsChecked == true) &&
                 cmbBox1.Text != null && lstCard.Count != 0)
@@ -79,11 +79,11 @@ namespace JsonBuilder
             else
                 btnSave.IsEnabled = false;
         }
-        private int GetID()
+        private int GetID() // Method for determining the auto-increment 
         {
             return mw.prs1.Count() > 0 ? mw.prs1.Max<Person>(x => x.ID) + 1 : 1;
         }
-        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        private void ButtonSave_Click(object sender, RoutedEventArgs e) // Button for save the entered data
         {
             if (flag == false)
             {
@@ -116,7 +116,7 @@ namespace JsonBuilder
             txtInputDateCard.Clear();
             Close();
         }
-        private void RadioButtonMan_Checked(object sender, RoutedEventArgs e)
+        private void RadioButtonMan_Checked(object sender, RoutedEventArgs e) // Radio button that allows you to select a male gender
         {
             if (rbtnMan.IsChecked == true && rbtnWoman.IsChecked == false)
                 ManMaritalStatus();
@@ -124,32 +124,32 @@ namespace JsonBuilder
                 WomanMaritalStatus();
 
         }
-        private void RadioButtonWoman_Checked(object sender, RoutedEventArgs e)
+        private void RadioButtonWoman_Checked(object sender, RoutedEventArgs e) // Radio button that allows you to select a female gender
         {
             if (rbtnWoman.IsChecked == true)
                 WomanMaritalStatus();
             else
                 ManMaritalStatus();
         }
-        private void ManMaritalStatus()
+        private void ManMaritalStatus() // Method for choosing the status (man)
         {
             cmbBox1.Items.Clear();
-            cmbBox1.Items.Add("Холост");
-            cmbBox1.Items.Add("Женат");
-            cmbBox1.Items.Add("Разведен");
+            cmbBox1.Items.Add("Single");
+            cmbBox1.Items.Add("Married");
+            cmbBox1.Items.Add("Divorced");
         }
-        private void WomanMaritalStatus()
+        private void WomanMaritalStatus() // Method for choosing the status (woman)
         {
             cmbBox1.Items.Clear();
-            cmbBox1.Items.Add("Замужем");
-            cmbBox1.Items.Add("Незамужем");
-            cmbBox1.Items.Add("Разведена");
+            cmbBox1.Items.Add("Married");
+            cmbBox1.Items.Add("Not married");
+            cmbBox1.Items.Add("Divorced");
         }
-        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e) // Method for closing the form
         {
             Close();
         }
-        private void txtCntChild_LostFocus(object sender, RoutedEventArgs e)
+        private void txtCntChild_LostFocus(object sender, RoutedEventArgs e) // Method for entering the count of children 
         {
             int count;
             string vvod = txtCntChild.Text;
@@ -166,7 +166,7 @@ namespace JsonBuilder
                 btnSave.IsEnabled = true;
             }
         }
-        private void btnCardAddOrEdit_Click(object sender, RoutedEventArgs e)
+        private void btnCardAddOrEdit_Click(object sender, RoutedEventArgs e) // Method for adding or editing a card
         {
             Card card = new Card(long.Parse(txtInputCard.Text.Replace(" ", "")), txtInputDateCard.Text);
             lstCard.Add(card);
@@ -175,12 +175,12 @@ namespace JsonBuilder
             txtInputDateCard.Clear();
             btnCardAddOrEdit.IsEnabled = false;
         }
-        private void btnCardDelete_Click(object sender, RoutedEventArgs e)
+        private void btnCardDelete_Click(object sender, RoutedEventArgs e) // Method for deleting the card
         {
             if (listCardAdd.Items.Count != 0)
             {
-                var res = System.Windows.MessageBox.Show("Вы действительно хотите удалить этот номер карты?",
-                                          "Удалить номер", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                var res = System.Windows.MessageBox.Show("Do you really want to delete this card number?",
+                                          "Delete number", MessageBoxButton.YesNo, MessageBoxImage.Question);
                 if (res == MessageBoxResult.Yes)
                 {
                     var line = listCardAdd.SelectedItems[0];
@@ -189,7 +189,7 @@ namespace JsonBuilder
                 }
             }
         }
-        private void txtInputCard_LostFocus(object sender, RoutedEventArgs e)
+        private void txtInputCard_LostFocus(object sender, RoutedEventArgs e) // Method for entering the card number
         {
             long number;
             string inputCard = txtInputCard.Text.Replace(" ", "");
@@ -203,23 +203,21 @@ namespace JsonBuilder
             {
                 txtInputCard.BorderBrush = new SolidColorBrush(Colors.White);
                 lbErrorCard.Visibility = Visibility.Hidden;
-                //!!!
                 flgFullCardNumber = true;
-                //btnCardAddOrEdit.IsEnabled = true;
             }
         }
-        private void listCardAdd_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void listCardAdd_SelectionChanged(object sender, SelectionChangedEventArgs e) // Method for removing a card from the list (if the card was selected)
         {
             if (listCardAdd.SelectedItem != null)
                 btnCardDelete.IsEnabled = true;
             else
                 btnCardDelete.IsEnabled = false;
         }
-        private void txtInputDateCard_LostFocus(object sender, RoutedEventArgs e)
+        private void txtInputDateCard_LostFocus(object sender, RoutedEventArgs e) // Method for entering the card expiration date
         {
             int date;
             string inputDateCard = txtInputDateCard.Text.Replace("/","");
-            if (!int.TryParse(inputDateCard, out date) || date > 1299) // 12/99 (12 месяцев, 99-й год максимум)
+            if (!int.TryParse(inputDateCard, out date) || date > 1299) // 12/99 (12 months, 99-th year max)
             {
                 txtInputDateCard.BorderBrush = new SolidColorBrush(Colors.Red);
                 lbErrorDate.Visibility = Visibility.Visible;
@@ -229,7 +227,6 @@ namespace JsonBuilder
             {
                 txtInputDateCard.BorderBrush = new SolidColorBrush(Colors.White);
                 lbErrorDate.Visibility = Visibility.Hidden;
-                //!!!
                 if (flgFullCardNumber == true)
                     btnCardAddOrEdit.IsEnabled = true;
             }
